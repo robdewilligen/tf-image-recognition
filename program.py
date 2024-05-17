f38405d5b0c0250dc841e194420e632063cf5462
Rob de Willigen <robdewilligen@outlook.com>
2024-05-17 10:47:18 +0200

    update script from bugs
    make sure to update your packages if the model performs shit

--- program.py
+++ program.py
@@ -1,52 +1,57 @@
 import pathlib
 
-import matplotlib.pyplot as plt
-import numpy as np
-import PIL
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers, Sequential
 
-
 # Tutorial
 # https://www.tensorflow.org/tutorials/images/classification
+
+# Some notes for troubleshooting i encountered
+# - If the model does not perform how you want it to and is over or underfitting like crazy, wipe your cache of pictures in "~/.keras/datasets"
+# - Check if all your packages are up to date, for me Keras was not updated and broke everything.
 
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file('flower_photos.tar', origin=dataset_url, extract=True)
 data_dir = pathlib.Path(data_dir).with_suffix('')
 
+# Print the amount of images
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
+# Set batch and image parameters
 batch_size = 32
 img_height = 180
 img_width = 180
 
+# create training and validation datasets with the dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.3,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.3,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
+# Print the class names in the training set
 class_names = train_ds.class_names
 print(class_names)
 
-for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
 
+for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
+
+# Tune the model
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
@@ -54,43 +59,43 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = len(class_names)
 
+# Add data augmentation for more files to process during training
 data_augmentation = keras.Sequential(
-  [
-    layers.RandomFlip("horizontal",
-                      input_shape=(img_height,
-                                   img_width,
-                                   3)),
-    layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1),
-  ]
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+    ]
 )
 
+# Create the model
 model = Sequential([
-  data_augmentation,
-  layers.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes, activation="softmax", name="Outputs")
+    layers.Input(shape=(img_height, img_width, 3)),
+    data_augmentation,
+    layers.Rescaling(1. / 255),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes, activation="softmax", name="Outputs")
 ])
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), # from_logits should be true if there is no softmax activation on the last Dense layer
               metrics=['accuracy'])
 
 model.summary()
 
-epochs=15
+epochs = 15
 history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
 )
 
 model.save('data/programmed_model.keras')
